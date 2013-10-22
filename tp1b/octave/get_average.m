5f0d80f34a0eda46c71f9c306996d703c6aad9aa
Juan Manuel Tastzian <jm@tast.com.ar>
2013-10-22 19:05:23 -0300

    Agrega la funcion que calcula el promedio de las matrices de traceroute OS

---
+++ tp1b/octave/get_average.m
@@ -0,0 +1,13 @@
+function average = get_average(city, franja)
+	if (nargin != 2)
+    printf('%s\n', "ERROR: La funcion recibe una ciudad y una franja como parámetro")
+    return
+  end
+
+  average = 0
+  matrix = load_matrix(city, franja)
+
+  column_mean = mean(matrix)
+  average = (column_mean(1) + column_mean(2) + column_mean(3))/3
+
+end
