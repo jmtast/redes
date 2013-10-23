b86441b2a93ec716ca4ac8f60ede1082c72c7ee0
root <mgiordano@dc.uba.ar>
2013-10-23 11:57:42 -0300

    cambios en scripts oct

---
+++ tp1b/octave/getMatTRPy.m
@@ -0,0 +1,65 @@
+%Este script genera la matriz de rtts promedidaos calculados por traceroute de python
+
+function M_tr_py = getMatTRPy
+
+	%Cargamos las franjas horarias
+	v_fr = load('../results/franjas.txt');
+
+	%Armamos la fila de California
+	v_tr_p = zeros(1,length(v_fr));
+
+	for i = 1:length(v_fr)
+		ciudad = '../results/rttCalifornia-';
+		if(v_fr(i) < 10)
+			ciudad = strcat(ciudad, "0");
+			ciudad = strcat(ciudad, mat2str(v_fr(i)));
+		else
+			ciudad = strcat(ciudad, mat2str(v_fr(i)));
+		end
+		ciudad = strcat(ciudad, 'hs.txt');
+		
+		A = load(ciudad);
+		rtts = A(:,2);
+		avg = sum(rtts) / length(rtts);
+		v_tr_p(i) = avg;
+	end
+
+	M_tr_py = [v_tr_p];
+
+	%Armamos la fila de Oxford
+	for i = 1:length(v_fr)
+		ciudad = '../results/rttOxford-';
+		if(v_fr(i) < 10)
+			ciudad = strcat(ciudad, "0");
+			ciudad = strcat(ciudad, mat2str(v_fr(i)));
+		else
+			ciudad = strcat(ciudad, mat2str(v_fr(i)));
+		end
+		ciudad = strcat(ciudad, 'hs.txt');
+		
+		A = load(ciudad);
+		rtts = A(:,2);
+		avg = sum(rtts) / length(rtts);
+		v_tr_p(i) = avg;
+	end
+
+	M_tr_py = [M_tr_py; v_tr_p];
+
+	for i = 1:length(v_fr)
+		ciudad = '../results/rttTokyo-';
+		if(v_fr(i) < 10)
+			ciudad = strcat(ciudad, "0");
+			ciudad = strcat(ciudad, mat2str(v_fr(i)));
+		else
+			ciudad = strcat(ciudad, mat2str(v_fr(i)));
+		end
+		ciudad = strcat(ciudad, 'hs.txt');
+		
+		A = load(ciudad);
+		rtts = A(:,2);
+		avg = sum(rtts) / length(rtts);
+		v_tr_p(i) = avg;
+	end
+
+	M_tr_py = [M_tr_py; v_tr_p];
+end
--- tp1b/octave/plot_franjas.m
+++ tp1b/octave/plot_franjas.m
@@ -1,20 +1,39 @@
-function plot_franjas(matriz)
+%Este script realiza el grafico de los rtt de tr_python y tr_linux promediados en funcion de las franjas horarias y graficando como constantes a los rtt teoricos.
 
-	if(nargin != 1)
-		printf("ERROR: la función recibe una matriz como parámetro)\n")
-	else
-		franjas = [0, 2, 8, 11, 15, 22];
-		plot(franjas, matriz(1,:), 'x');
+function plot_franjas
+
+		%vector de franjas, eje x
+		franjas = load('../results/franjas.txt');
+
+		%matriz de promedios tr_linux
+		M_tr_l = getMatTRLin();
+
+		%matriz de promedios tr_python
+		M_tr_p = getMatTRPy();
+
+		%puntos rtt linux
+		plot(franjas, M_tr_l(1,:), '@o1');
+		hold on;
+		plot(franjas, M_tr_l(2,:), '@x1');
+		hold on;
+		plot(franjas, M_tr_l(3,:), '@*1');
+		hold on;
+		
+		%puntos rtt python
+		plot(franjas, M_tr_p(1,:), '@o3');
+		hold on;
+		plot(franjas, M_tr_p(2,:), '@x3');
+		hold on;
+		plot(franjas, M_tr_p(3,:), '@*3');
 		hold on;
 
-		plot(franjas, matriz(2,:), 'o');
-		plot(franjas, matriz(3,:), '+');
+		%rtt teoricos como constantes
 
-		xlabel('Franjas horarias');
+		xlabel('Franjas horarias (hs)');
 		ylabel('Roundtrip Time (ms)');
-		title('Roundtrip Time a lo largo del dia');
-		legend('Berkeley', 'Oxford', 'Tokio');
-
+		title('RTT traceroute propio vs tr. linux a lo largo del dia');
+		legend('RTT Berkeley Lx', 'RTT Oxford Lx', 'RTT Tokio Lx', 'RTT Berkeley Py', 'RTT Oxford Py', 'RTT Tokio Py', "location", "southeast");
+		legend("boxon");
+		axis([-0.5 24]);
 		hold off;
-	end
 end
